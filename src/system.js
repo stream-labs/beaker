101a41e2b638320e6c0ebf4313d187ebff894c06
mbiemiller <morgan@streamlabs.com>
2018-11-05 17:46:05 -0800

    more updates, setting up exports

--- src/system.js
+++ src/system.js
@@ -6,27 +6,40 @@
  */
 
 // Define contexts to require
-const contexts = [
-  require.context("@/components/", true, /\.vue$/)
-]
+const contexts = require.context("@/components/", true, /\.vue$/);
+
+import Accordion from "@/components/Accordion";
+import Button from "@/components/Button";
+import FormGroupH from "@/components/FormGroupH";
+import FormGroupV from "@/components/FormGroupV";
+import ProgressBadge from "@/components/ProgressBadge";
+import Slider from "@/components/Slider";
+import Selector from "@/components/Selector";
+import Tabs from "@/components/Tabs";
 
 // Define components
-const components = [];
-contexts.forEach(context => {
-  context.keys().forEach(key => components.push(context(key).default));
-});
+const components = [
+  Accordion,
+  Button,
+  FormGroupH,
+  FormGroupV,
+  ProgressBadge,
+  Selector,
+  Slider,
+  Tabs
+];
 
 // Install the above defined components
-const System = {
-  install(Vue) {
-    components.forEach(component => Vue.component(component.name, component));
-  }
-};
+// const System = {
+//   install(Vue) {
+//     components.forEach(component => Vue.component(component.name, component));
+//   }
+// };
 
 // Automatic installation if Vue has been added to the global scope
-if (typeof window !== "undefined" && window.Vue) {
-  window.Vue.use(System);
-}
+// if (typeof window !== "undefined" && window.Vue) {
+//   window.Vue.use(System);
+// }
 
 // Finally export as default
 export default {
