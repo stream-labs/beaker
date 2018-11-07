31401c94618ce22a66aa05f9d110eb6574b4b1e4
joshua-larks <joshua.larks@streamlabs.com>
2018-11-07 12:51:20 -0800

    Update callout section descriptions

---
+++ src/components/Callout.vue
@@ -0,0 +1,139 @@
+<template>
+  <div
+    class="callout"
+    :class="[ calloutClass ]">
+    <i
+      v-if="icon"
+      class="fas"
+      :class="[calloutIcon]"></i>
+    <slot/>
+    <i v-if="closeable" class="fas fa-times callout__close"></i>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Callout extends Vue {
+  @Prop({ default: "success" })
+  variation!: string;
+
+  @Prop({ default: true })
+  icon!: boolean;
+
+  @Prop({ default: false })
+  closeable!: boolean;
+
+  get calloutClass() {
+    return `callout--${this.variation}`;
+  }
+
+  get calloutIcon() {
+    switch (this.variation) {
+      case "success":
+      case "success-alt":
+        return "fa-check-circle";
+      case "warning":
+      case "warning-alt":
+        return "fa-times-circle";
+      case "info":
+        return "fa-info-circle";
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import "./../styles/Imports";
+
+.callout {
+  position: relative;
+  .margin-bottom(3);
+  .padding();
+  border: none;
+  .radius();
+
+  a {
+    font-weight: normal;
+    text-decoration: underline;
+    color: inherit;
+  }
+
+  .fab,
+  .far,
+  .fas {
+    font-size: 18px;
+    vertical-align: text-top;
+    margin-right: 6px;
+  }
+
+  .callout__close {
+    font-size: 12px;
+    color: inherit;
+    top: 12px;
+    right: 10px;
+    opacity: .5;
+    position: absolute;
+    .transition();
+    cursor: pointer;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+
+  .inline-button {
+    .weight(@medium);
+    margin-left: 0;
+    text-decoration: none;
+  }
+
+  .button {
+    margin-top: 10px;
+  }
+
+  .close-button {
+    opacity: .6;
+    .transition();
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+}
+
+.callout--success {
+  background: @teal;
+  color: @white;
+
+  a,
+  .button--default {
+    color: @white;
+  }
+}
+
+.callout--success-alt {
+  background: rgba(50, 195, 162, 0.16);
+  color: @teal;
+}
+
+.callout--warning {
+  background: @warning;
+  color: @white;
+}
+
+.callout--warning-alt {
+  color: @warning;
+  background: @red-semi;
+}
+
+.callout--info {
+  background: #FEF9E7;
+  color: #C49E34;
+
+  a {
+    color: #C49E34;
+  }
+}
+</style>
