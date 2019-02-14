dcd550aa612727f5eda06c61a0fbf2de5d869b54
neferent <neferentlive@gmail.com>
2019-02-14 15:35:28 -0800

    uh oh...

--- src/demos/ColorPickers.vue
+++ src/demos/ColorPickers.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="section">
-    <h2>Color Picker</h2>
-    <color-picker name="colors" :value="colors" v-model="colors"></color-picker>
+    <h1>Color Picker</h1>
+    <color-picker v-model="colors"></color-picker>
   </div>
 </template>
 
@@ -18,10 +18,9 @@ import ColorPicker from "./../components/ColorPicker.vue";
 })
 export default class ColorPickers extends Vue {
   colors = {
-    hex: "#194d33",
-    hsl: { h: 150, s: 0.5, l: 0.2, a: 1 },
-    hsv: { h: 150, s: 0.66, v: 0.3, a: 1 },
-    rgba: { r: 25, g: 77, b: 51, a: 1 },
+    hex: "#31c3a2",
+    hsl: { h: 166, s: 0.6, l: 0.48, a: 1 },
+    rgba: { r: 49, g: 195, b: 162, a: 1 },
     a: 1
   };
 }
