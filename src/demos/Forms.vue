1c4b45e5abd8acf946f8f7646b13f6b4b7c051ee
mbiemiller <morgan@streamlabs.com>
2019-03-25 15:06:57 -0700

    lint

--- src/demos/Forms.vue
+++ src/demos/Forms.vue
@@ -21,7 +21,7 @@ import PaymentForm from "./../components/PaymentForm.vue";
 
 @Component({
   components: {
-    PaymentForm,
+    PaymentForm
     // FormGroupH,
     // FormGroupV,
     // TextInput
