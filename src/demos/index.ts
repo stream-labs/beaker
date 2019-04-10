0f579064183a8ad0444874a1713fe96e960aa06d
Tom Maneri <tommaneri@gmail.com>
2019-04-10 13:20:01 -0700

    should fix build/dist

--- src/demos/index.ts
+++ src/demos/index.ts
@@ -4,8 +4,12 @@ export default demos.keys().map(key => {
   const componentName = key.slice(2, -4);
   const label = componentName.split(/(?=[A-Z])/).join(" ");
   const name = label.replace(/ /g, "-").toLowerCase();
+  const context = demos(key);
 
   return {
+    // component: () =>
+    //   import(/* webpackChunkName: "view-[request]" */ `@/demos/${componentName}.vue`),
+    component: context.default,
     name,
     componentName,
     label
