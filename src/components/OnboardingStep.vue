d5f669616ef5cd86ed230cb432e0f40770f6fdb6
Rei Yoshizawa <reiyoshizawa0418@gmail.com>
2019-04-12 14:17:49 -0700

    Remove

--- src/components/OnboardingStep.vue
+++ src/components/OnboardingStep.vue
@@ -4,52 +4,6 @@
     <p><slot name="desc"></slot></p>
     <slot></slot>
   </div>
-  <!-- <modal
-    name="new-feature"
-    :adaptive="true"
-    :width="'100%'"
-    :height="'auto'"
-    classes="s-overlay__wrapper"
-    :clickToClose="true"
-    @opened="opened"
-  >
-    <div slot="top-right" class="s-overlay__icon">
-      <span class="s-icon icon-close" @click="onDismiss"></span>
-    </div>
-    <div class="s-overlay__container" :class="containerMq">
-      <div class="s-overlay__body">
-        <p class="s-overlay__label">{{ label }}</p>
-        <h1 class="s-overlay__title">{{ title }}</h1>
-        <p class="s-overlay__text">
-          <slot></slot>
-        </p>
-        <div class="s-overlay-links">
-          <Button
-            :size="'large'"
-            :variation="'action'"
-            :tag="buttonTag"
-            :to="buttonRoute"
-            :href="buttonHref"
-            :target="buttonTarget"
-            :title="buttonTitle"
-            @click.native="onPrimaryAction"
-          ></Button>
-          <router-link
-            class="s-overlay__link"
-            :to="dismissRoute"
-            @click.native="onDismiss"
-          >{{ dismissText }}</router-link>
-        </div>
-      </div>
-
-      <div class="s-overlay__image-block" :class="overlay__imageBlockMq">
-        <img v-if="isImage" :src="overlayImage" class="s-overlay__image">
-        <video controls="false" autoplay loop v-if="!isImage" class="s-overlay__image">
-          <source :src="overlayImage">Environment does not support video playback
-        </video>
-      </div>
-    </div>
-  </modal>-->
 </template>
 
 <script lang="ts">
@@ -69,8 +23,3 @@ export default class OnboardingStep extends Vue {
   desc!: string;
 }
 </script>
-
-<style lang="less" scoped>
-@import "./../styles/Imports";
-
-</style>
