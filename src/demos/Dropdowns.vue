7e140f407894310645ea4716dbddfda99507bd39
neferent <neferentlive@gmail.com>
2019-05-13 08:49:57 -0700

    fixed scrollbar jumping issue

--- src/demos/Dropdowns.vue
+++ src/demos/Dropdowns.vue
@@ -9,7 +9,13 @@
       <Accordion :openedTitle="'Hide Code'" :closedTitle="'Show Code'">
         <div slot="content">
           <pre>
-<code>&lt;pane-dropdown&gt;
+<code>import { Accordion } from 'streamlabs-beaker';
+
+components: {
+  Accordion
+}
+
+&lt;pane-dropdown&gt;
   &lt;template slot=&quot;title&quot;&gt;Trending&lt;/template&gt;
   &lt;a href=&quot;#&quot;&gt;Add&lt;/a&gt;
   &lt;a href=&quot;#&quot;&gt;Update&lt;/a&gt;
@@ -17,42 +23,41 @@
 &lt;/pane-dropdown&gt;</code></pre>
         </div>
       </Accordion>
-
       <pane-dropdown>
-        <template slot="title"
-          >Trending</template
-        >
-        <a href="#"> <i class="icon-check"></i>Most Installed </a>
-        <a href="#"> <i class="icon-delete"></i>Newly Added </a>
+        <template slot="title">Trending</template>
+        <a href="#">
+          <i class="icon-check"></i>Most Installed
+        </a>
+        <a href="#">
+          <i class="icon-delete"></i>Newly Added
+        </a>
         <a href="#">Trending</a>
       </pane-dropdown>
     </div>
-
     <div class="section">
       <h3>Menu Align</h3>
-
       <pane-dropdown :menuAlign="'right'">
         <span slot="title">Right Dropdown Menu</span>
         <a href="#">Hey I'm a longer link here</a>
         <a href="#">Let's see how this looks</a>
         <a href="#">I'm in a pane dropdown aligned to the right</a>
       </pane-dropdown>
-      <br />
-      <br />
+      <br>
+      <br>
       <pane-dropdown :menuAlign="'center'">
         <span slot="title">Center Dropdown Menu</span>
         <a href="#">Hey I'm a longer link here</a>
         <a href="#">Let's see how this looks</a>
         <a href="#">I'm in a pane dropdown aligned to the right</a>
       </pane-dropdown>
-      <br />
-      <br />
+      <br>
+      <br>
       <pane-dropdown :custom="true">
         <span slot="title">Custom Dropdown Menu</span>
         <div>
           hey
           <span>watsup</span>
-          <br />
+          <br>
           <b>put whatever you want in here</b>
         </div>
       </pane-dropdown>
@@ -82,7 +87,8 @@
             <td>null</td>
             <td>
               Default will align the menu to the left side of the toggle. Use
-              <code>right</code> or <code>center</code> to align the dropdown
+              <code>right</code> or
+              <code>center</code> to align the dropdown
               menu to the right or center of its toggle.
             </td>
           </tr>
