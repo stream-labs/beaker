7e6b53bd4a54176c85675b428a0af1e04a48de50
AnkhHeart <yours-truly@hotmail.be>
2019-06-05 06:38:15 +0200

    Tagging Input & Fixes
    - Added Util to attach all listeners down to <input
    - Fixed v-model two way update using @watch in textinput
    - Created a TaggingInput that allows entry of tags
    - Added Vee-Validate

---
+++ src/util/Util.ts
@@ -0,0 +1,21 @@
+export class Util {
+  static attachListeners(listeners: Record<string, Function | Function[]>, target: any, exclude: string[] = []) {
+    for (const key in listeners) {
+      if (listeners.hasOwnProperty(key) && exclude.indexOf(key) === -1) {
+        const func = listeners[key];
+
+        target.addEventListener(key, func);
+      }
+    }
+  }
+
+  static detachListeners(listeners: Record<string, Function | Function[]>, target: any) {
+    for (const key in listeners) {
+      if (listeners.hasOwnProperty(key)) {
+        const func = listeners[key];
+
+        target.removeEventListener(key, func);
+      }
+    }
+  }
+}
