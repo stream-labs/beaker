078c54bc43eae8b5b14e98162e3119de15e009e1
joshua-larks <joshua.larks@streamlabs.com>
2019-06-05 11:45:38 -0700

    create discord banner vue file

---
+++ src/components/BannerDiscord.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Join the Streamlabs OBS Discussion on <span>Discord</span></p>
+    <Button
+      title="Open Discord"
+      :bgColor="'#FFFFFF'"
+      :textColor="'#7289DA'"
+    ></Button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Button from "./../components/Button.vue";
+
+@Component({
+  components: {
+    Button
+  }
+})
+export default class BannerDiscord extends Vue {}
+</script>
