38245393e1ddae6030bf6056507565f26c3fe4fb
AnkhHeart <yours-truly@hotmail.be>
2019-06-06 06:27:45 +0200

    TextInput Number support
    - Added a basic Spinner & Scroll behaviour when type is number for TextInput

--- src/demos/Inputs.vue
+++ src/demos/Inputs.vue
@@ -20,6 +20,17 @@
         ></text-input>
 
         <text-input
+          label="Number input"
+          type="number"
+          :placeholder="textInputPlaceholder"
+          v-model="numberInputValue"
+          v-validate="'required|between:-1,100'"
+          name="numberinputExample"
+          slot="input"
+          :error="errors.first('numberinputExample')"
+        ></text-input>
+
+        <text-input
           label="Email input"
           type="email"
           :placeholder="emailInputPlaceholder"
@@ -62,9 +73,7 @@
             <td>disabled</td>
             <td>boolean</td>
             <td>null</td>
-            <td>
-              Puts a disabled class on the form field and disables the input.
-            </td>
+            <td>Puts a disabled class on the form field and disables the input.</td>
           </tr>
           <tr>
             <td>error</td>
@@ -99,7 +108,9 @@
             <td>text</td>
             <td>
               Input type:
-              <code>text</code>, <code>email</code>, <code>password</code>.
+              <code>text</code>,
+              <code>email</code>,
+              <code>password</code>.
             </td>
           </tr>
           <tr>
@@ -152,11 +163,8 @@ components: {
 &gt;&lt;/selector&gt;</code></pre>
         </div>
       </Accordion>
-      <Selector
-        v-model="selected"
-        :options="['Option A', 'Option B', 'Option C']"
-      ></Selector>
-      <br />
+      <Selector v-model="selected" :options="['Option A', 'Option B', 'Option C']"></Selector>
+      <br>
       <selector
         v-model="selected"
         :options="[
@@ -169,7 +177,7 @@ components: {
         ]"
         disabled
       ></selector>
-      <br />
+      <br>
       <selector
         v-model="multipleSelected"
         :options="[
@@ -183,7 +191,7 @@ components: {
         multiple
         :searchable="false"
       ></selector>
-      <br />
+      <br>
       <selector
         v-model="multipleSelected"
         :options="[
@@ -197,15 +205,10 @@ components: {
         disabled
         multiple
       ></selector>
-      <br />
+      <br>
 
-      <selector
-        v-model="objectSelected"
-        :options="options"
-        :label="'title'"
-        :trackBy="'title'"
-      ></selector>
-      <br />
+      <selector v-model="objectSelected" :options="options" :label="'title'" :trackBy="'title'"></selector>
+      <br>
 
       <table class="docs-table">
         <thead>
@@ -251,9 +254,7 @@ components: {
             <td>disabled</td>
             <td>Boolean</td>
             <td>false</td>
-            <td>
-              Puts a disabled class on the form field and disables the input.
-            </td>
+            <td>Puts a disabled class on the form field and disables the input.</td>
           </tr>
         </tbody>
       </table>
@@ -323,10 +324,7 @@ components: {
     <div class="section">
       <h2>Status Switch</h2>
       <div class="section">
-        <status-switch
-          v-model="statusValue"
-          :label="'Status Label'"
-        ></status-switch>
+        <status-switch v-model="statusValue" :label="'Status Label'"></status-switch>
       </div>
       <div class="section">
         <small>
@@ -344,16 +342,12 @@ components: {
       </div>
 
       <div class="section">
-        <status-switch v-model="statusValue" />
-        <status-switch v-model="statusValue" />
-        <status-switch v-model="statusValue" />
+        <status-switch v-model="statusValue"/>
+        <status-switch v-model="statusValue"/>
+        <status-switch v-model="statusValue"/>
       </div>
 
-      <Accordion
-        :openedTitle="'Hide Code'"
-        :closedTitle="'Show Code'"
-        class="docs-code"
-      >
+      <Accordion :openedTitle="'Hide Code'" :closedTitle="'Show Code'" class="docs-code">
         <div slot="content">
           <pre><code>&lt;status-switch v-model="statusValue" :label="'Status Label'"&gt;&lt;/status-switch&gt;
 
@@ -384,11 +378,7 @@ components: {
           By default, the image input picker is setup to have our Alert Box
           layout styles as the options.
         </p>
-        <Accordion
-          :openedTitle="'Hide Code'"
-          :closedTitle="'Show Code'"
-          class="docs-code"
-        >
+        <Accordion :openedTitle="'Hide Code'" :closedTitle="'Show Code'" class="docs-code">
           <div slot="content">
             <pre>
 <code>&lt;image-picker-input v-model=&quot;layoutValue&quot;&gt;&lt;/image-picker-input&gt;</code></pre>
@@ -402,20 +392,13 @@ components: {
           You can pass in any options you'd like with the options prop. This is
           an example using our Jar options.
         </p>
-        <Accordion
-          :openedTitle="'Hide Code'"
-          :closedTitle="'Show Code'"
-          class="docs-code"
-        >
+        <Accordion :openedTitle="'Hide Code'" :closedTitle="'Show Code'" class="docs-code">
           <div slot="content">
             <pre>
 <code>&lt;image-picker-input v-model=&quot;jarValue&quot; :options=&quot;options&quot;&gt;&lt;/image-picker-input&gt;</code></pre>
           </div>
         </Accordion>
-        <image-picker-input
-          v-model="jarValue"
-          :options="options"
-        ></image-picker-input>
+        <image-picker-input v-model="jarValue" :options="options"></image-picker-input>
       </div>
     </div>
 
@@ -485,6 +468,7 @@ export default class Inputs extends Vue {
 
   // Text inputs
   textInputValue = "test";
+  numberInputValue = "0";
   emailInputValue = "";
   passwordInputValue = "";
   errorTextInputValue = "";
