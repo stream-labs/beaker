6f61580f97787811986c7114ff42be8f9d35edd3
Joshua Larks <joshua.larks@streamlabs.com>
2019-08-07 10:39:31 -0700

    Change style size from em to pixel to match beaker mocks (#387)

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="s-call-to-action" :class="callToActionMq" :style="callToActionBg">
     <i
-      v-if="buttonVariation === 'prime' | buttonVariation ==='prime-white'"
+      v-if="(buttonVariation === 'prime') | (buttonVariation === 'prime-white')"
       class="icon-prime prime-bg"
     ></i>
     <div
@@ -13,8 +13,12 @@
       <img :src="thumbnail" />
     </div>
     <div class="s-call-to-action__description" :class="callToActionDescMq">
-      <div class="s-title" :class="titleMq" :style="callToActiontitleColor">{{ title }}</div>
-      <div class="s-subtitle" :style="callToActionSubTitleColor">{{ description }}</div>
+      <div class="s-title" :class="titleMq" :style="callToActiontitleColor">
+        {{ title }}
+      </div>
+      <div class="s-subtitle" :style="callToActionSubTitleColor">
+        {{ description }}
+      </div>
     </div>
     <slot v-if="customButtonSlot"></slot>
     <div v-else class="s-button-container s-button-container--right">
