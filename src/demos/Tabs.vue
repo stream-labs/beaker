4d2a007d4ca7da8e9b3e17a9e2ac578729c2ff9b
Joshua Larks <joshua.larks@streamlabs.com>
2020-01-21 16:48:47 -0800

    feat(newTabs): add full arrow key accessibility

--- src/demos/Tabs.vue
+++ src/demos/Tabs.vue
@@ -51,10 +51,7 @@ components: {
           <td>
             tabs information to display. you can make array like below
             <br />
-            <code>
-              tabs = [ { name: "General", value: "general", icon: "information"
-              }
-            </code>
+            <code>tabs = [ { name: "General", value: "general", icon: "information" }, ... ];</code>
           </td>
         </tr>
         <tr>
