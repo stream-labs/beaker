e79f7a9ba29697c601849acb7dcd2cd979640b08
Rei Yoshizawa <ryoshizawa@corp.logitech.com>
2020-10-27 12:27:31 -0700

    feat: create the reusable banner using slots
    Naming slot is applied in most of the parts since slot is more reusable,
    customizable than prop. And all of the elements are optional including
    background image.
    
    Also, banners are listed on the same view now instead of using tabs. No
    need to use tabs for only displaying a few banners each tab.

---
+++ src/components/BannerIntroduction.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="s-banner-introduction" :style="{ 'background-color': bgColor }">
+    <slot name="bgImage" />
+    <div class="s-banner-introduction__body">
+      <h2 class="s-banner-introduction__title">
+        <slot name="title" />
+      </h2>
+      <p class="s-banner-introduction__description">
+        <slot name="description" />
+      </p>
+    </div>
+    <div class="s-banner-introduction__button">
+      <slot name="button" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class BannerIntroduction extends Vue {
+  @Prop(String)
+  bgColor!: string;
+}
+</script>
+
+<style lang="less">
+@import (reference) "./../styles/Imports";
+
+.s-banner-introduction {
+  box-shadow: 0px 2px 8px rgba(9, 22, 29, 0.16);
+  border-radius: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 16px;
+  position: relative;
+
+  &__title,
+  &__description {
+    margin: 0;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    line-height: 130%;
+    color: @white !important;
+  }
+
+  &__title {
+    font-size: 16px;
+  }
+
+  &__description {
+    .margin-top();
+    font-size: 14px;
+  }
+
+  &__button {
+    button {
+      cursor: pointer;
+    }
+  }
+}
+</style>
