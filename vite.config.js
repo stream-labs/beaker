f7480970654b68081f385042a386796e96fd815f
joshua-larks <jlarks@logitech.com>
2022-11-21 13:09:24 -0600

    Update Vite config - Add separate config for github pages and npm builds - Add separte build commands to package.json

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import { defineConfig } from 'vite';
+import { createVuePlugin } from 'vite-plugin-vue2';
 
-const path = require("path");
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/beaker/',
   plugins: [createVuePlugin()],
   build: {
     commonjsOptions: {
-      requireReturnsDefault: true
-    }
+      requireReturnsDefault: true,
+    },
   }
 })
